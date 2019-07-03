ea89936bb0e1c44f73e0aaf65e9df427cc7d8062
minatoyukina <1096445518@qq.com>
2019-07-03 22:31:54 +0800

    thread abc

---
+++ src/main/java/concurrent/abc/ABC1.java
@@ -0,0 +1,54 @@
+package concurrent.abc;
+
+public class ABC1 {
+    private static int count = 1;
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (lock) {
+                while (count < 99) {
+                    while (count % 3 != 1) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(count++ + Thread.currentThread().getName());
+                    lock.notifyAll();
+                }
+            }
+        }, "A").start();
+        new Thread(() -> {
+            synchronized (lock) {
+                while (count < 99) {
+                    while (count % 3 != 2) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(count++ + Thread.currentThread().getName());
+                    lock.notifyAll();
+                }
+            }
+        }, "B").start();
+        new Thread(() -> {
+            synchronized (lock) {
+                while (count < 99) {
+                    while (count % 3 != 0) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(count++ + Thread.currentThread().getName());
+                    lock.notifyAll();
+                }
+            }
+        }, "C").start();
+    }
+}
